29a8082fd2d600297ef8a352492f4771ee02a8d8
Mrxinkuncai <759180302@qq.com>
2022-03-13 10:40:03 +0800

    增加requirements文件

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
 print("hello world!")
+
+
+import pytest
+import allure
+
+
+class TestCase1:
+
+    @allure.story("demo1")
+    def test_1_demo1(self):
+        print("demo1")
+
+    @allure.story("demo2")
+    def test_1_demo2(self):
+        print("demo2")
